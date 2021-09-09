b2c8d8c079a432bcb59d4c3f5076a4189baebc66
Adam Fletcher <adam@bit.io>
2021-09-09 14:13:51 -0700

    Add example file

---
+++ BitioExample.java
@@ -0,0 +1,42 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class BitioExample {
+   public static void main(String args[]) {
+      Connection c = null;
+			String bitApiKey = "<your api key>";
+		  String bitDB = "sensors";
+		  String bitUser = "adam";
+			String bitHost = "db.bit.io";
+			String bitPort = "5432"; // We keep this as a string here as we are concact'ing it into the connection string
+      Properties props = new Properties();
+      props.setProperty("sslmode","require");
+      props.setProperty("user",bitUser);
+      props.setProperty("password",bitApiKey);
+      try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager
+            .getConnection("jdbc:postgresql://" + bitHost + ":" + bitPort + "/" + bitDB, props);
+         Statement stmt = c.createStatement();
+         ResultSet rs = stmt.executeQuery("SELECT * FROM \"adam/sensors\".\"measurements\" order by datetime desc;" );
+         while (rs.next()) {
+             ResultSetMetaData rsmd = rs.getMetaData();
+             // The ResultSet .getXXX() methods expect the column index to start at 1. 
+             // No idea why.
+             for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+               System.out.print(rsmd.getColumnName(i) + "="+ rs.getString(i) + " ");
+             }
+             System.out.println();
+         }
+
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+   }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bitdotio-java-example
 An example of using Java &amp; JDBC with bit.io
+
+First, download the latest version of the [Postgres JDBC](https://jdbc.postgresql.org/) driver, and
+put that jar file in this directory.
+
+Then, to run this code (replace the version of the postgres jar with the version you downloaded): 
+```
+javac BitioExample.java
+java -cp .:postgresql-42.2.23.jar BitioExample
+```
+
